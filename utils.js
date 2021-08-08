a80c04da32b98ee8956c57845818e9fe8c7b4cf8
Yoav Hirshberg <yoav.yh@gmail.com>
2021-08-08 16:49:37 +0300

    improving performance of script loadingreformating files

--- utils.js
+++ utils.js
@@ -1,48 +1,7 @@
-function copyMat(mat) {
-    var newMat = [];
-    for (var i = 0; i < mat.length; i++) {
-        newMat[i] = [];
-        for (var j = 0; j < mat[0].length; j++) {
-            newMat[i][j] = mat[i][j];
-        }
-    }
-    return newMat;
-};
-
-function countNeighbor(cellI, cellJ, mat) {
-    var neighborsCount = 0;
-    for (var i = cellI - 1; i <= cellI + 1; i++) {
-        if (i < 0 || i >= mat.length) continue;
-        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
-            if (i === cellI && j === cellJ) continue;
-            if (j < 0 || j >= mat[i].length) continue;
-            // if (mat[i][j] === LIFE || mat[i][j] === SUPER_LIFE) neighborsCount++;
-            if (mat[i][j]) neighborsCount++;
-        }
-    }
-    return neighborsCount;
-};
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is inclusive and the minimum is inclusive 
-};
+'use strict';
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-  }
-
-function shuffle(items) {
-    var randIdx, keep, i;
-    for (i = items.length - 1; i > 0; i--) {
-        randIdx = getRandomInt(0, items.length - 1);
-
-        keep = items[i];
-        items[i] = items[randIdx];
-        items[randIdx] = keep;
-    }
-    return items;
-};
+}
